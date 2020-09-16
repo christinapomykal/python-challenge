a771a81dde25670ef61ce3ef1ff849056de26de9
christinapomykal <christinapomykal@gmail.com>
2020-09-15 22:33:39 -0400

    Progress on net total in PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,28 +1,65 @@
+# Import os and csv modules
+
 import os
 import csv
 
 csvpath = os.path.join("Resources", "budget_data.csv")
+# print(csvpath)
+
+# Lists to store data
+date = []
+profitLoss = []
 
 with open(csvpath) as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=",")
+    # print(csvreader)
 
-    #print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
+    # Read header row
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    # print(f"CSV Header: {csv_header}")
 
-    totalMonths=0
+    # Read each row of data after the header
+    for row in csvreader:
+        
+        # Add date
+        date.append(row[0])
 
-    #totalMonths=len(list(csvreader))
+        # Add profitLoss
+        profitLoss.append(row[1])
+    
+# Done reading csvreader. Data in lists. 
+
+print("Total Months: " + str(len(date)))
+
+# for x in profitLoss:
+#     print(x)
+
+netTotal = 0
+
+for i in range(len(profitLoss)):
+    print(str(i) + "--" + str(profitLoss[i]))
+    netTotal = netTotal + int(profitLoss[i]) # is the same as: netTotal += profitLoss[i]
+    print(str(netTotal))
+
+print("Grand Total: " + str(netTotal))
+
+    
+
+    # print(len(list(csvreader)))
+    # totalMonths=0
+
+    # totalMonths=len(list(csvreader))
+    
+    # print(totalMonths)
 
     #print(totalMonths, " = total number of months in dataset")
 
     # Read each row of data after the header
 
-    for row in csvreader:
-        #print(row)
-        print(row[1])
-        totalMonths=totalMonths+1
+    # for row in csvreader:
+    #     #print(row)
+    #     print(row[1])
+    #     totalMonths=totalMonths+1
 
-    print(totalMonths)
+    # print(totalMonths)
