2242cc6e315b05cf3b6395e320a3909097137193
christinapomykal <christinapomykal@gmail.com>
2020-09-16 22:37:37 -0400

    coded average change in PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,16 +21,12 @@ with open(csvpath) as csvfile:
 
     # Read each row of data after the header
     for row in csvreader:
-        
         # Add date
         date.append(row[0])
-
         # Add profitLoss
         profitLoss.append(row[1])
     
 # Done reading csvreader. Data in lists. 
-
-print("Total Months: " + str(len(date)))
 
 # for x in profitLoss:
 #     print(x)
@@ -44,54 +40,36 @@ deltaList = []
 
 for i in range(len(profitLoss)):
     # print(str(i) + "--" + str(profitLoss[i]) + "-- On: " + date[i])
-    # is the same as: netTotal += profitLoss[i]
-    netTotal = netTotal + int(profitLoss[i])
+    
+    # is the same as: netTotal = netTotal + int(profitLoss[i])
+    netTotal += int(profitLoss[i])
     # print(str(netTotal))
 
     if i > 0:
+        # conditional to start at index 1, calculate delta from month to month
         currentDelta = int(profitLoss[i]) - int(profitLoss[i - 1])
         
         # adding values to deltaList
         deltaList.append(currentDelta) 
         # print("Current Delta: " + str(currentDelta))
+        
+        # compare the deltas and keep the larger for profits
         if currentDelta > biggestDelta:
             biggestDelta = currentDelta
             biggestDeltaDate = date[i]
-
-
         
+        # compare the deltas and keep the smaller for losses
         if currentDelta < biggestLoss:
             biggestLoss = currentDelta
             biggestLossDate = date[i]
 
-avgChange = 
+avgChange = sum(deltaList) / len(deltaList)
 
-print("Grand Total: " + str(netTotal))
-
-print("Biggest Delta Date: " + str(biggestDeltaDate))
-print("Biggest Delta: " + str(biggestDelta))
-print("Biggest Loss Date: " + str(biggestLossDate))
+print("Financial Analysis")
+print("--------------------------------")
+print("Total Months: " + str(len(date)))
+print("Grand Total: $" + str(netTotal))
+print("Average Change: $" + str(round(avgChange, 2)))
+print("Greatest Increase in Profits: " + str(biggestDeltaDate) + " ($" + str(biggestDelta) + ")")
+print("Greatest Decrease in Profits: " + str(biggestLossDate) + " ($" + str(biggestLoss) + ")")
 print("Biggest Loss: " + str(biggestLoss))
-    
-
-
-
-    
-
-    # print(len(list(csvreader)))
-    # totalMonths=0
-
-    # totalMonths=len(list(csvreader))
-    
-    # print(totalMonths)
-
-    #print(totalMonths, " = total number of months in dataset")
-
-    # Read each row of data after the header
-
-    # for row in csvreader:
-    #     #print(row)
-    #     print(row[1])
-    #     totalMonths=totalMonths+1
-
-    # print(totalMonths)
