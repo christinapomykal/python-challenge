edb078db771d4093cdccfbf5f05bb445a00544da
christinapomykal <christinapomykal@gmail.com>
2020-09-18 19:36:40 -0400

    add election results PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,10 +8,8 @@ csvpath = os.path.join("Resources", "election_data.csv")
 
 # Lists or dictionaries?
 totalVote = 0
-uniqueCandName = []
 # tally is a dict = {uniqueCandName: candsTotalVotes}
 tally = {}
-
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -31,9 +29,41 @@ with open(csvpath) as csvfile:
             # add value 1 vote if uniqueCandName already a key in tally dict
             tally[row[2]] += 1 
             
-# After for loop, tally dict now filled with unique keys (uniqueCandName) with the associated value (candsTotalVotes)
-print(tally)
+winnerVote = 0
+winnerName = ""
+# voter_output = []
+print("Election Results")
+print("--------------------")
+print(f"Total Votes: {totalVote}")
+print("--------------------")
 
-# Now loop through the dict, and do the math per key:value in dict
-# find % by pull value from the dict for that key / totalVote
-# Winner is comparing all the values with the key
+# After for loop, tally dict now filled with unique keys (uniqueCandName) with the associated value (candsTotalVotes)
+for uniqueCandName in tally: 
+    
+    percentCand = (tally[uniqueCandName] / totalVote * 100)
+    voter_output = f"{uniqueCandName}: {percentCand:.3f}% ({tally[uniqueCandName]})"
+    print(voter_output)
+
+    if tally[uniqueCandName] > winnerVote: 
+        winnerVote = tally[uniqueCandName]
+        winnerName = uniqueCandName
+
+    
+print("--------------------")
+print(f"Winner: {winnerName}")
+print("--------------------")
+
+output_path = os.path.join("Analysis", "pyPollAnalysis.txt")
+
+with open(output_path, "w") as txtfile:
+    txtfile.write("Election Results\n")
+    txtfile.write("--------------------\n")
+    txtfile.write(f"Total Votes: {totalVote}\n")
+    txtfile.write("--------------------\n")
+    for uniqueCandName in tally: 
+        percentCand = (tally[uniqueCandName] / totalVote * 100)
+        voter_output = f"{uniqueCandName}: {percentCand:.3f}% ({tally[uniqueCandName]})"
+        txtfile.write(f"{voter_output}\n")
+    txtfile.write("--------------------\n")
+    txtfile.write(f"Winner: {winnerName}\n")
+    txtfile.write("--------------------\n")
